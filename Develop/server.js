a8a6af93cc4dde69bba735d673c89e5f84a03156
joe <hudakjoe@msn.com>
2022-01-22 18:21:55 -0500

    server running

---
+++ Develop/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static('public'));
+
+// api route
+// html route
+app.listen(port, () => {
+  console.log('running');
+});

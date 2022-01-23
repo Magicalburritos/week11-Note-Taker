d777674f4bf8df0255be4613e5b5bc79e20fe381
joe <hudakjoe@msn.com>
2022-01-22 19:39:23 -0500

    got notes to show up

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
 const util = require('util');
 const router = require('express').Router();
-const notes = require('../db/db.json');
 
 const writeFileAsync = util.promisify(fs.writeFile);
 const readFileAsync = util.promisify(fs.readFile);
 
 router.get('/notes', (req, res) => {
-  readFileAsync(notes, 'utf-8')
+  readFileAsync('./db/db.json', 'utf-8')
     .then((data) => {
       res.json(JSON.parse(data));
     })
